46afe9d52b5d3a8bf69e338f3fd95bc520e0e5ee
Tân <ngoctana1k2014@gmail.com>
2021-10-07 03:19:55 +0700

    them 1 vai modal va phan login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,44 @@
-const modal = document.getElementById('modal_1')
-const modalClose = document.getElementById('modal__close')
+// Heart animation
+const heart = document.getElementById('heart')
+const heartAnimation = bodymovin.loadAnimation({
+    container: heart,
+    path: 'https://assets1.lottiefiles.com/datafiles/nZgj7wTd56UtH6m/data.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+})
+
+// Modal 1
+const modal_1 = document.getElementById('modal-1')
+const modalClose_1 = document.getElementById('modal-1-close')
 const btnYes = document.getElementById('btn-yes')
 
-modalClose.addEventListener('click', () => {
-    modal.classList.remove('active')
+modalClose_1.addEventListener('click', () => {
+    modal_1.classList.remove('active')
 })
 
 btnYes.addEventListener('click', () => {
-    modal.classList.add('active')
+    modal_1.classList.add('active')
+    heartAnimation.goToAndPlay(0, true)
+})
+
+// Modal 2
+const modal_2 = document.getElementById('modal-2')
+const btnOk = document.getElementById('card-ok')
+
+btnOk.addEventListener('click', () => {
+    modal_1.classList.remove('active')
+    setTimeout(() => {
+        modal_2.classList.add('active')
+    }, 200)
+})
+
+// Login
+const login = document.getElementById('card-login')
+const formLogin = document.getElementById('form-login')
+login.addEventListener('click', () => {
+    modal_2.classList.remove('active')
+    setTimeout(() => {
+        formLogin.classList.add('active')
+    }, 200)
 })
