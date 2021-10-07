49ee5b01de14a447e5ea453e7f8d2c987dffa9a5
Tân <ngoctana1k2014@gmail.com>
2021-10-07 16:25:17 +0700

    move button yes and no

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,10 +33,39 @@ const heartFlyingAnimation_3 = bodymovin.loadAnimation({
     autoplay: false,
 })
 
+// Yes/No
+const btnYes = document.getElementById('btn-yes')
+const btnNo = document.getElementById('btn-no')
+const title = document.getElementById('title')
+
+btnNo.addEventListener('click', () => {
+    const bodyWidth = document.body.clientWidth
+    const bodyHeight = document.body.clientHeight
+    const btnWidth = btnNo.offsetWidth
+    const btnHeight = btnNo.offsetHeight
+    const titleHeight = title.offsetHeight + title.offsetTop
+    const oldLeft = btnNo.offsetLeft
+    const oldTop = btnNo.offsetTop
+    let newLeft = Math.abs(Math.floor(Math.random() * bodyWidth - btnWidth))
+    let newTop = Math.abs(Math.floor(Math.random() * bodyHeight - btnHeight))
+
+    while(newLeft < btnWidth || newLeft + btnWidth >= bodyWidth || newLeft >= oldLeft - btnWidth && newLeft <= oldLeft + btnWidth) {
+        newLeft = Math.abs(Math.floor(Math.random() * bodyWidth - btnWidth))
+    }
+    while(newTop < titleHeight || newTop + btnHeight >= bodyHeight || newTop >= oldTop - btnHeight && newTop <= oldTop + btnHeight) {
+        newTop = Math.abs(Math.floor(Math.random() * bodyHeight - btnHeight))
+    }
+
+    btnNo.style.left = newLeft + 'px'
+    btnNo.style.top = newTop + 'px'
+
+    btnYes.style.left = oldLeft + 'px'
+    btnYes.style.top = oldTop + 'px'
+})
+
 // Modal 1
 const modal_1 = document.getElementById('modal-1')
 const modalClose_1 = document.getElementById('modal-1-close')
-const btnYes = document.getElementById('btn-yes')
 
 modalClose_1.addEventListener('click', () => {
     modal_1.classList.remove('active')
