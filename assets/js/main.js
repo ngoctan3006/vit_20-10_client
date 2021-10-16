633e3f420e200f024185b865aaa870e3c257b129
Tân <ngoctana1k2014@gmail.com>
2021-10-16 16:43:26 +0700

    load data from api

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,19 +139,20 @@ $('#form-login').on('submit', function (e) {
                     formNotify.innerText = ''
                     const cf = confirm('Tài khoản không tồn tại? Tiếp tục đăng nhập với tư cách khác?')
                     if(cf) {
-                        window.sessionStorage.setItem('fullName', '')
-                        window.sessionStorage.setItem('image', [])
-                        window.sessionStorage.setItem('wish', [])
+                        window.localStorage.setItem('fullName', '')
+                        window.localStorage.setItem('image_h', '')
+                        window.localStorage.setItem('image_v', '')
+                        window.localStorage.setItem('wish', '')
                         window.location = '../../gift.html'
                     }
                     break;
                 case 'Success':
                     console.log(data.image_h)
                     console.log(data.image_v)
-                    window.sessionStorage.setItem('fullName', data.fullName)
-                    window.sessionStorage.setItem('image_h', data.image_h)
-                    window.sessionStorage.setItem('image_v', data.image_v)
-                    window.sessionStorage.setItem('wish', data.wish)
+                    window.localStorage.setItem('fullName', JSON.stringify(data.fullName))
+                    window.localStorage.setItem('image_h', JSON.stringify(data.image_h))
+                    window.localStorage.setItem('image_v', JSON.stringify(data.image_v))
+                    window.localStorage.setItem('wish', JSON.stringify(data.wish))
                     window.location = '../../gift.html'
                     break;
                 default:
